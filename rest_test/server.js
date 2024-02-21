cab5b27e67d1843e50b31bdf57dd741653ef632b
dinsorkot <mju372ohm@gmail.com>
2024-02-21 22:30:58 +0700

    book

---
+++ rest_test/server.js
@@ -0,0 +1,42 @@
+// server.js
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+const books = require('./db')
+app.get('/books', (req, res) => {
+  res.json(books)
+})
+/*
+app.get('/books/:id', (req, res) => {
+    res.json(books.find(book => book.id === req.params.id))
+})
+
+app.put('/books/:id', (req, res) => {
+    const updateIndex = books.findIndex(book => book.id === req.params.id);
+    res.json(Object.assign(books[updateIndex], req.body))
+})
+
+app.delete('/books/:id', (req, res) => {
+    const deletedIndex = books.findIndex(book => book.id === req.params.id)
+    delete books[deletedIndex];
+    res.status(200).json(req.body)
+})
+*/
+app.listen(3000, () => {
+  console.log('http://localhost:3000')
+})
+/*
+app.post('/books', (req, res) => {
+    books.push(req.body)
+    res.status(200).json(req.body)
+  })
+*/
